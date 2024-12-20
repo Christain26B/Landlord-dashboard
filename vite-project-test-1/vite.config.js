96fcdadc8dd71c02441ec50c761a28dc9c995033
Christian26B <christianbrown2613@gmail.com>
2024-12-20 16:51:31 +0000

    rent calc

--- vite-project-test-1/vite.config.js
+++
@@ -1,9 +0,0 @@
-// this is the config file which talks to the compiler, we never need to touch this 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
